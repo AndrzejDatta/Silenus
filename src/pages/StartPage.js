5c7326434a83b5dc651ee95fac5f79025c3ade18
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-05 23:46:14 +0200

    create StartPage page

---
+++ src/pages/StartPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StartPageHeader from "components/organisms/StartPageHeader/StartPageHeader";
+const StartPage = (props) => {
+  return <StartPageHeader startPage="startPage" size="small" />;
+};
+
+StartPage.propTypes = {};
+
+export default StartPage;
