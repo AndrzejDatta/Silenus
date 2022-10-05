2ecdd0e65f8a330382d67daf40eac4f283f6eb3a
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-05 23:46:30 +0200

    change default value of prop color

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledLogo } from "components/atoms/Logo/Logo.styles";
 
-const Logo = ({ color = "black", size = "small" }) => {
+const Logo = ({ color = "#3AA688", size = "small" }) => {
   return <StyledLogo fill={color} size={size} />;
 };
 
