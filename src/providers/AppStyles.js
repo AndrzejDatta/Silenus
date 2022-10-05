9a383cb23febb1e689a206b712d3edd16e4637fd
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-05 21:58:22 +0200

    add ThemeProvider, GlobalStyle

---
+++ src/providers/AppStyles.js
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "assets/styles/theme";
+import { GlobalStyle } from "assets/styles/GlobalStyles";
+export const AppStyles = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  );
+};
