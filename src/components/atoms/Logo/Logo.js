0edb5ce80ad4b16cfdac55edb206366b27f23cc5
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-05 22:36:22 +0200

    relocate Logo styles to Logo.styles.js

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -1,28 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ReactComponent as logo } from "assets/icons/logo.svg";
-import styled from "styled-components";
+import { StyledLogo } from "components/atoms/Logo/Logo.styles";
 
-const StyledLogo = styled(logo)`
-  ${(props) => {
-    if (props.size === "small") {
-      return `
-            width:150px;
-            height:150px;
-            `;
-    } else if (props.size === "medium") {
-      return `
-            width:200px;
-            height:200px;
-            `;
-    } else if (props.size === "large") {
-      return `
-            width:250px;
-            height:250px;
-            `;
-    }
-  }}
-`;
 const Logo = ({ color = "black", size = "small" }) => {
   return <StyledLogo fill={color} size={size} />;
 };
