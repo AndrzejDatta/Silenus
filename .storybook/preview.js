56fac7a43c529c5a91ffc181947dc7c21c3fc9e9
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-05 21:58:48 +0200

    add decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "assets/styles/theme";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
