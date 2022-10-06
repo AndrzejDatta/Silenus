9a957d27e192939a5737ed81211a8c07c7a87409
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-06 11:47:39 +0200

    create template

---
+++ src/components/templates/StartTemplate/StartTemplate.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import useWindowDimensions from "hooks/useWindowDimensions";
+import { size } from "helpers/breakpoints";
+import { StyledStartTemplate } from "./StartTemplate.styles";
+import StartPageHeader from "components/organisms/StartPageHeader/StartPageHeader";
+const StartTemplate = () => {
+  const { width } = useWindowDimensions();
+
+  let elementSize;
+  if (width < size.mobileS) {
+    elementSize = "small";
+  } else if (width < size.tablet) {
+    elementSize = "medium";
+  } else if (width >= size.laptop) {
+    elementSize = "medium";
+  }
+  return (
+    width && (
+      <StyledStartTemplate>
+        <StartPageHeader size={elementSize} startPage="startPage" />
+      </StyledStartTemplate>
+    )
+  );
+};
+
+StartTemplate.propTypes = {};
+
+export default StartTemplate;
---
+++ src/components/templates/StartTemplate/StartTemplate.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledStartTemplate = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+`;
