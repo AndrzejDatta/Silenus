4fbafac20ee81623c0fcbd64743780c5a8e9dd80
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-10 23:27:32 +0200

    create data.js file with informations

---
+++ src/assets/data/data.js
@@ -0,0 +1,139 @@
+export const dataPL = [
+  {
+    pageName: "StartPage",
+    title: "Sielnus",
+    subtitle: "smart plants",
+  },
+  {
+    pageName: "LoginPage",
+    title: "Sielnus",
+    subtitle: "smart plants",
+    infoText: "Nie masz jeszcze konta?",
+    linkText: "Załóż konto",
+  },
+  {
+    pageName: "Dashboard",
+    headlings: ["Moje doniczki", "Moje rośliny", "Kalendarz opieki"],
+    filters: ["Podlewanie", "Zraszanie", "Nawożenie"],
+  },
+  {
+    pageName: "MojeDoniczki",
+    headlings: [
+      "Moje doniczki",
+      "Parametry doniczki",
+      "Pielęgnacja",
+      "Pamietnik rośliny",
+    ],
+  },
+  {
+    pageName: "DodajDoniczke",
+    headlings: ["Dodaj doniczkę"],
+    infoText: "Wyszukiwanie urządzenia bluetooth w pobliżu",
+    buttonText: "Skanuj",
+  },
+  {
+    pageName: "MojeRośliny",
+    headlings: ["Moje rośliny", "Pielęgnacja", "Pamietnik rośliny"],
+    filters: ["Podlewanie", "Zraszanie", "Nawożenie"],
+  },
+  {
+    pageName: "KalendarzOpieki",
+    headlings: ["Profil", "Ustawienia"],
+    options: [
+      "Edytuj profil",
+      "Udostępnij",
+      "Powiadomienia",
+      "Zabezpieczenia",
+      "Pomoc",
+      "O aplikacji",
+      "Oceń",
+      "Wyloguj sie",
+    ],
+  },
+  {
+    pageName: "KontoUżytkownika",
+    headlings: ["Kalendarz opieki"],
+    filters: ["Podlewanie", "Zraszanie", "Nawożenie"],
+  },
+  {
+    pageName: "ZidentyfikujRoslinę",
+    headlings: ["identyfikuj roslinę", "Popularne kategorie"],
+    filters: [
+      "bezpieczne dla zwierząt",
+      "rośliny zielone",
+      "rosliny jonizujące",
+      "rośliny biurowe",
+    ],
+    buttonText: "Zrób zdjęcie",
+  },
+];
+
+export const dataAng = [
+  {
+    pageName: "StartPage",
+    title: "Sielnus",
+    subtitle: "smart plants",
+  },
+  {
+    pageName: "LoginPage",
+    title: "Sielnus",
+    subtitle: "smart plants",
+    infoText: "Nie masz jeszcze konta?",
+    linkText: "Załóż konto",
+  },
+  {
+    pageName: "Dashboard",
+    headlings: ["Moje doniczki", "Moje rośliny", "Kalendarz opieki"],
+    filters: ["Podlewanie", "Zraszanie", "Nawożenie"],
+  },
+  {
+    pageName: "MojeDoniczki",
+    headlings: [
+      "Moje doniczki",
+      "Parametry doniczki",
+      "Pielęgnacja",
+      "Pamietnik rośliny",
+    ],
+  },
+  {
+    pageName: "DodajDoniczke",
+    headlings: ["Dodaj doniczkę"],
+    infoText: "Wyszukiwanie urządzenia bluetooth w pobliżu",
+    buttonText: "Skanuj",
+  },
+  {
+    pageName: "MojeRośliny",
+    headlings: ["Moje rośliny", "Pielęgnacja", "Pamietnik rośliny"],
+    filters: ["Podlewanie", "Zraszanie", "Nawożenie"],
+  },
+  {
+    pageName: "KalendarzOpieki",
+    headlings: ["Profil", "Ustawienia"],
+    options: [
+      "Edytuj profil",
+      "Udostępnij",
+      "Powiadomienia",
+      "Zabezpieczenia",
+      "Pomoc",
+      "O aplikacji",
+      "Oceń",
+      "Wyloguj sie",
+    ],
+  },
+  {
+    pageName: "KontoUżytkownika",
+    headlings: ["Kalendarz opieki"],
+    filters: ["Podlewanie", "Zraszanie", "Nawożenie"],
+  },
+  {
+    pageName: "ZidentyfikujRoslinę",
+    headlings: ["identyfikuj roslinę", "Popularne kategorie"],
+    filters: [
+      "bezpieczne dla zwierząt",
+      "rośliny zielone",
+      "rosliny jonizujące",
+      "rośliny biurowe",
+    ],
+    buttonText: "Zrób zdjęcie",
+  },
+];
