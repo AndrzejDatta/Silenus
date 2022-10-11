d0ba25f5b54cd13503b8ec5f94a95b134944137b
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-11 12:01:10 +0200

    add DataProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppStyles } from "providers/AppStyles";
 import App from "./App";
-
+import { DataProvider } from "providers/DataProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppStyles>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </AppStyles>
   </React.StrictMode>
 );
