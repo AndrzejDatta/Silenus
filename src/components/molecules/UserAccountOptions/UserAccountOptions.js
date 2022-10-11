81c5b062b9d570afdcc655f3ccd0cfa419fe2f25
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-11 13:56:46 +0200

    modify map process

--- src/components/molecules/UserAccountOptions/UserAccountOptions.js
+++ src/components/molecules/UserAccountOptions/UserAccountOptions.js
@@ -2,33 +2,55 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { dataContext } from "providers/DataProvider";
 import UserAccountText from "components/atoms/UserAccountText/UserAccountText";
+import Icon from "components/atoms/icon/icon.component";
+import { StyledUserAccountOptions } from "./UserAccountOptions.styles";
+
 const UserAccountOptions = (props) => {
   const {
-    KontoUżytkownika: { options },
+    KontoUżytkownika: { options, headings },
+    images,
   } = useContext(dataContext);
-  const optionsElement = options?.map((item) => {
-    if (item === "Wyloguj się") {
+  const optionsElement = options?.map(({ option, iconSrc }) => {
+    if (option === "Wyloguj się") {
       return (
-        <UserAccountText
-          text={item}
-          isLogout={true}
-          isHeadling={false}
-          isOption={false}
-        />
+        <StyledUserAccountOptions>
+          <Icon icon={images[iconSrc]} size="small" color="black" />
+          <UserAccountText
+            text={option}
+            isLogout={true}
+            isHeadling={false}
+            isOption={false}
+          />
+        </StyledUserAccountOptions>
       );
     } else {
       return (
-        <UserAccountText
-          text={item}
-          isLogout={false}
-          isHeadling={false}
-          isOption={true}
-        />
+        <StyledUserAccountOptions>
+          <Icon icon={images[iconSrc]} size="small" color="green" />
+          <UserAccountText
+            text={option}
+            isLogout={false}
+            isHeadling={false}
+            isOption={true}
+          />
+        </StyledUserAccountOptions>
       );
     }
   });
+  const headingElements = headings?.map((item) => {
+    return (
+      <UserAccountText
+        text={item}
+        isLogout={false}
+        isHeadling={true}
+        isOption={false}
+      />
+    );
+  });
+  optionsElement.splice(0, 0, headingElements[0]);
+  optionsElement.splice(3, 0, headingElements[1]);
 
-  return optionsElement;
+  return <>{optionsElement}</>;
 };
 
 UserAccountOptions.propTypes = {};
---
+++ src/components/molecules/UserAccountOptions/UserAccountOptions.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledUserAccountOptions = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
