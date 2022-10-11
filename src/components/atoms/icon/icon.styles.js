a0c842ea44a8f8110597e33aa3c4e8ee63b42106
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-11 13:58:53 +0200

    add option to filter green

--- src/components/atoms/icon/icon.styles.js
+++ src/components/atoms/icon/icon.styles.js
@@ -1,14 +1,14 @@
-import { theme } from 'assets/styles/theme';
-import styled from 'styled-components';
+import { theme } from "assets/styles/theme";
+import styled from "styled-components";
 
 export const StyledIcon = styled.img`
   ${(props) => {
-    if (props.size === 'small') {
+    if (props.size === "small") {
       return `
             width: 24px;
             height: 24px; 
         `;
-    } else if (props.size === 'medium') {
+    } else if (props.size === "medium") {
       return `
             width: 27px;
             height: 27px;
@@ -16,10 +16,12 @@ export const StyledIcon = styled.img`
     }
   }};
   ${(props) => {
-    if (props.color === 'black') {
+    if (props.color === "black") {
       return `filter: ${theme.color.filterBlack}`;
-    } else if (props.color === 'white') {
+    } else if (props.color === "white") {
       return `filter: ${theme.color.filterWhite}`;
+    } else if (props.color === "green") {
+      return `filter: ${theme.color.filterGreen}`;
     }
   }};
   cursor: pointer;
