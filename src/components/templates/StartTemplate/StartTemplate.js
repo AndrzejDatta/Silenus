15c6c34a404abfd90cc66c046b671906ee879faf
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-12 16:21:26 +0200

    change way in which prop logoSize is giving

--- src/components/templates/StartTemplate/StartTemplate.js
+++ src/components/templates/StartTemplate/StartTemplate.js
@@ -4,25 +4,13 @@ import useWindowDimensions from "hooks/useWindowDimensions";
 import { size } from "helpers/breakpoints";
 import { StyledStartTemplate } from "./StartTemplate.styles";
 import StartPageHeader from "components/organisms/StartPageHeader/StartPageHeader";
-const StartTemplate = ({ children, pageType, logoColor }) => {
+const StartTemplate = ({ children, pageType, logoColor, logoSize }) => {
   const { width } = useWindowDimensions();
 
-  let elementSize;
-  if (width <= size.mobileS && pageType === "loginPage") {
-    console.log("tiny");
-    elementSize = "tiny";
-  } else if (width < size.mobileS) {
-    console.log("small");
-    elementSize = "small";
-  } else if (width < size.tablet) {
-    elementSize = "medium";
-  } else if (width >= size.laptop) {
-    elementSize = "medium";
-  }
   return (
     <StyledStartTemplate pageType={pageType}>
       <StartPageHeader
-        logoSize={elementSize}
+        logoSize={logoSize}
         pageType={pageType}
         logoColor={logoColor}
       />
