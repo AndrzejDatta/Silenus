f6ad3231d06d694695f0ef6efba02a1a38a57cc7
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-12 15:40:50 +0200

    try to fix bug

--- src/components/atoms/Subtitle/Subtitle.js
+++ src/components/atoms/Subtitle/Subtitle.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyledSubtitle } from 'components/atoms/Subtitle/Subtitle.styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { StyledSubtitle } from "components/atoms/Subtitle/Subtitle.styles";
 const Subtitle = ({ subtitle, pageType }) => {
   return <StyledSubtitle pageType={pageType}>{subtitle}</StyledSubtitle>;
 };
 Subtitle.propTypes = {
   /**text in Subtitle atom*/
   subtitle: PropTypes.string.isRequired,
+  /** type pf page */
   pageType: PropTypes.string.isRequired,
 };
 
--- src/components/atoms/Subtitle/Subtitle.styles.js
+++ src/components/atoms/Subtitle/Subtitle.styles.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledSubtitle = styled.h2`
   ${({ pageType }) => {
-    if (pageType === 'loginPage') {
+    if (pageType === "loginPage") {
       return `
         font-size: ${({ theme }) => theme.font.size.big};
         color: white;
         `;
-    } else if (pageType === 'startPage') {
+    } else if (pageType === "startPage") {
       return `
         font-size: ${({ theme }) => theme.font.size.subtitle};
         color: ${({ theme }) => theme.color.fontGreen};
