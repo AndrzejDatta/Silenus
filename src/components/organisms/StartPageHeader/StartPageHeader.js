574ed758e7c1767ef647904fb3bd66982c091748
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-12 15:38:45 +0200

    fix bug

--- src/components/organisms/StartPageHeader/StartPageHeader.js
+++ src/components/organisms/StartPageHeader/StartPageHeader.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyledStartPageHeader } from 'components/organisms/StartPageHeader/StartPageHeader.styles';
-import Logo from 'components/atoms/Logo/Logo';
-import StartPageText from 'components/molecules/StartPageText/StartPageText';
-const StartPageHeader = ({ logoSize, logoColor }) => {
+import React from "react";
+import PropTypes from "prop-types";
+import { StyledStartPageHeader } from "components/organisms/StartPageHeader/StartPageHeader.styles";
+import Logo from "components/atoms/Logo/Logo";
+import StartPageText from "components/molecules/StartPageText/StartPageText";
+const StartPageHeader = ({ logoSize, logoColor, pageType }) => {
   return (
     <StyledStartPageHeader>
       <Logo logoSize={logoSize} color={logoColor} />
-      <StartPageText pageType={'loginPage'} />
+      <StartPageText pageType={pageType} />
     </StyledStartPageHeader>
   );
 };
