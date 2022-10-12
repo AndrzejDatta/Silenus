8fd2069a3d42eedc7dace75199f56a35cec16074
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-12 12:38:47 +0200

    add div and add styles of options

--- src/components/molecules/UserAccountOptions/UserAccountOptions.styles.js
+++ src/components/molecules/UserAccountOptions/UserAccountOptions.styles.js
@@ -4,4 +4,16 @@ export const StyledUserAccountOptions = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  &:nth-child(3) {
+    padding-bottom: 8px;
+  }
+  &:last-child {
+    padding-top: 10px;
+  }
+`;
+export const StyledUserAccountOptionsSection = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: flex-start;
 `;
