a8a448b13d9a96866802d49e4e7da3572c4f5a37
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-13 18:42:50 +0200

    create MyPlant molecule

---
+++ src/components/molecules/MyPlant/MyPlant.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Img from "components/atoms/Img/Img";
+import { StyledPlantText } from "./MyPlant.styles";
+const MyPlant = ({ src, name = "Plant" }) => {
+  return (
+    <Img url={src} size="small">
+      <StyledPlantText>{name}</StyledPlantText>
+    </Img>
+  );
+};
+
+MyPlant.propTypes = {
+  src: PropTypes.string,
+  name: PropTypes.string,
+};
+
+export default MyPlant;
