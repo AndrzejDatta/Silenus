4d2988630d48c329f577e9eb39a22c8205497634
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-13 21:06:41 +0200

    add div

--- src/components/organisms/PlantSection/PlantSection.styles.js
+++ src/components/organisms/PlantSection/PlantSection.styles.js
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 export const StyledPlantSection = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
+`;
+
+export const StyledPlantElementsSection = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
 `;
