d211868b5f9352efa9d2f5bdd4f38d054a327116
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-13 19:28:36 +0200

    add svg

--- src/components/atoms/HeadlineText/HeadlineText.js
+++ src/components/atoms/HeadlineText/HeadlineText.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StyledHeadlineText } from "./HeadlineText.styles";
-const HeadlineText = ({ text = "Healine" }) => {
-  return <StyledHeadlineText>{text}</StyledHeadlineText>;
+const HeadlineText = ({ text = "Healine", isBackground }) => {
+  return (
+    <StyledHeadlineText isBackground={isBackground}>{text}</StyledHeadlineText>
+  );
 };
 
 HeadlineText.propTypes = {
