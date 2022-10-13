67c17a642f45eb4771479a752a8ec898f8d98ee9
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-13 12:00:20 +0200

    rebuild structure of PotStatus

--- src/components/molecules/PotStatus/PotStatus.js
+++ src/components/molecules/PotStatus/PotStatus.js
@@ -9,46 +9,39 @@ import {
 import { dataContext } from "providers/DataProvider";
 import Icon from "components/atoms/icon/icon.component";
 import Img from "components/atoms/Img/Img";
-const PotStatus = (props) => {
-  const {
-    potIcons,
-    Dashboard: { plants },
-  } = useContext(dataContext);
+const PotStatus = ({ state, name }) => {
+  const { potIcons } = useContext(dataContext);
 
-  console.log(plants);
-  console.log(potIcons);
-  const elements = plants?.map(({ state, name }) => {
-    return (
-      <StyledPotStatus>
-        <Img size="small" src={"1"} />
-        <StyledPotStatusInfo>
-          <UserAccountText
-            text={name}
-            isLogout={false}
-            isHeadling={true}
-            isOption={false}
-            isUser={true}
-          />
-          {state.map(({ src, text }) => {
-            return (
-              <StyledPotStatusText>
-                <Icon icon={potIcons[src]} size="tiny" color="green" />
-                <UserAccountText
-                  text={text}
-                  isLogout={false}
-                  isHeadling={false}
-                  isOption={true}
-                />
-              </StyledPotStatusText>
-            );
-          })}
-        </StyledPotStatusInfo>
-      </StyledPotStatus>
-    );
-  });
+  const element = (
+    <StyledPotStatus>
+      <Img size="small" src={"1"} />
+      <StyledPotStatusInfo>
+        <UserAccountText
+          text={name}
+          isLogout={false}
+          isHeadling={true}
+          isOption={false}
+          isUser={true}
+        />
+        {state.map(({ src, text }) => {
+          return (
+            <StyledPotStatusText>
+              <Icon icon={potIcons[src]} size="tiny" color="green" />
+              <UserAccountText
+                text={text}
+                isLogout={false}
+                isHeadling={false}
+                isOption={true}
+              />
+            </StyledPotStatusText>
+          );
+        })}
+      </StyledPotStatusInfo>
+    </StyledPotStatus>
+  );
 
-  console.log(elements);
-  return elements;
+  console.log(element);
+  return element;
 };
 
 PotStatus.propTypes = {};
