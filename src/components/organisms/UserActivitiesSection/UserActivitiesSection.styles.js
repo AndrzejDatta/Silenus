6f8c579b2aca6df81890e05297d87cd344c0b6a7
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-14 21:36:38 +0200

    create UserActivitiesSection organism

---
+++ src/components/organisms/UserActivitiesSection/UserActivitiesSection.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledUserActivitySection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const StyledUserActivityElement = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-around;
+  width: 100%;
+`;
