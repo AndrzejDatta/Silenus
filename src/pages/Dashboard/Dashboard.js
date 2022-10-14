875bc48ead3d9c0dc2de1996d299952c4a8e328f
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-14 23:01:06 +0200

    add SearchBar molecule

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { dataContext } from "providers/DataProvider";
+import SearchBar from "components/molecules/SearchBar/SearchBar";
 import MainTemplate from "components/templates/MainTemplate/MainTemplate";
 import PlantSection from "components/organisms/PlantSection/PlantSection";
 import PotSection from "components/organisms/PotSection/PotSection";
@@ -12,6 +13,7 @@ const Dashboard = () => {
 
   return (
     <MainTemplate inBackground={true}>
+      <SearchBar isSearchBar={true} />
       <PotSection headline={headings[0]} />
       <PlantSection headline={headings[1]} />
       <UserActivitiesSection headline={headings[2]} />
