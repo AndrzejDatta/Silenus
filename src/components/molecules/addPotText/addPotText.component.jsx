2b00d54eb5183a8a8e7bde59f129d2b0269bba54
AndrzejDatta <andrzej.datta1@gmail.com>
2022-10-16 07:39:38 +0200

    [AD]: addPotText fix

--- src/components/molecules/addPotText/addPotText.component.jsx
+++ src/components/molecules/addPotText/addPotText.component.jsx
@@ -2,18 +2,21 @@ import Subtitle from 'components/atoms/Subtitle/Subtitle';
 import Title from 'components/atoms/Title/Title';
 import { dataContext } from 'providers/DataProvider';
 import React, { useContext } from 'react';
+import { StyledAddPotText } from './addPotText.styles';
 
 const AddPotText = () => {
-  debugger;
+  // debugger;
   const {
     DodajDoniczke: { infoText, headings },
   } = useContext(dataContext);
 
+  console.log(infoText);
+  console.log(headings);
   return (
-    <AddPotText>
+    <StyledAddPotText>
       <Title title={headings} pageType="loginPage" />
       <Subtitle subtitle={infoText} pageType="loginPage" />
-    </AddPotText>
+    </StyledAddPotText>
   );
 };
 
--- src/components/molecules/addPotText/addPotText.styles.js
+++ src/components/molecules/addPotText/addPotText.styles.js
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledAddPotText = styled.div``;
+export const StyledAddPotText = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
