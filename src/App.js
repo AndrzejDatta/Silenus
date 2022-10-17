bc3fe49e4c55589763a283af69a43e67c226d785
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-17 21:21:16 +0200

    add AddSection molecule

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import StartPage from "pages/StartPage/StartPage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from "pages/loginPage/loginPage.component";
 import Dashboard from "pages/Dashboard/Dashboard";
+import AddPot from "pages/AddPot/AddPot";
 import IdentifyPage from "pages/IdentifyPage/IdentifyPage";
 import UserAccount from "pages/UserAccount/UserAccount";
 import Calendar from "pages/Calendar/Calendar";
+import MyPlantsPage from "pages/MyPlantsPage/MyPlantsPage";
+import MyPotsPage from "pages/MyPotsPage/MyPotsPage";
 const App = (props) => {
   return (
     <BrowserRouter>
@@ -15,6 +18,9 @@ const App = (props) => {
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/camera" element={<IdentifyPage />} />
         <Route path="/user" element={<UserAccount />} />
+        <Route path="/mypots" element={<MyPotsPage />} />
+        <Route path="/myplants" element={<MyPlantsPage />} />
+        <Route path="/addpot" element={<AddPot />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/molecules/AddSection/AddSection.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledAddSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
