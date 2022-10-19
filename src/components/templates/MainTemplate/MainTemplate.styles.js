f3cd81e17170bd5289ac94ad48c83d38b0cd7af8
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-19 15:18:47 +0200

    fix bug

--- src/components/templates/MainTemplate/MainTemplate.styles.js
+++ src/components/templates/MainTemplate/MainTemplate.styles.js
@@ -23,3 +23,12 @@ export const StyledMainTemplate = styled.div`
     }
   }};
 `;
+
+export const StyledGradientCircle = styled.div`
+  position: absolute;
+  height: 150vw;
+  width: 200vw;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  background-image: linear-gradient(0deg, #42a686 12.36%, #5a92cb 100%);
+`;
