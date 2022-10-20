0a5c7dddac25a0f28a5e8833564dc59aed510aee
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-10-21 00:10:51 +0200

    clean code

--- src/components/organisms/StartPageHeader/StartPageHeader.js
+++ src/components/organisms/StartPageHeader/StartPageHeader.js
@@ -4,7 +4,6 @@ import { StyledStartPageHeader } from "components/organisms/StartPageHeader/Star
 import Logo from "components/atoms/Logo/Logo";
 import StartPageText from "components/molecules/StartPageText/StartPageText";
 const StartPageHeader = ({ logoSize, logoColor, pageType }) => {
-  console.log(logoSize);
   return (
     <StyledStartPageHeader>
       <Logo logoSize={logoSize} color={logoColor} />
