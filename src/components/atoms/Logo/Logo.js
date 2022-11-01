99884d754e348d58a723525d3d7db3a358f73879
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-11-02 00:03:57 +0100

    create Logo snapshot, add defaultProps

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -2,10 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledLogo } from "components/atoms/Logo/Logo.styles";
 
-const Logo = ({ color = "#3AA688", logoSize = "small" }) => {
+const Logo = ({ color, logoSize }) => {
   return <StyledLogo fill={color} logoSize={logoSize} />;
 };
 
+Logo.defaultProps = {
+  color: "#3AA688",
+  logoSize: "small",
+};
 Logo.propTypes = {
   /** color of logo */
   color: PropTypes.string.isRequired,
