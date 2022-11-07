f7c0a5775fa1d6b693cb5b162506ce3643a0d8f5
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-11-07 21:26:48 +0100

    set up configuration of storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'assets/styles/theme';
-
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "assets/styles/theme";
+import { DataProvider } from "providers/DataProvider";
+import { AppStyles } from "providers/AppStyles";
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -14,8 +15,10 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <ThemeProvider theme={theme}>
-      <Story />
-    </ThemeProvider>
+    <AppStyles>
+      <DataProvider>
+        <Story />
+      </DataProvider>
+    </AppStyles>
   ),
 ];
