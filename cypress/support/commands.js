2a2c6ca5d75ceafa4e9c77d28fdc52109d41179f
Aleksandra Borowska <olkaborowska333@wp.pl>
2022-11-07 21:17:55 +0100

    install cypress, react test library, msw, jest

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("clickElement", (datacy) => {
+  cy.get(`[data-cy='${datacy}']`).first().click();
+});
+Cypress.Commands.add("clickNavElement", (datacy) => {
+  cy.get(`[data-cy='${datacy}Nav']`).first().click();
+});
